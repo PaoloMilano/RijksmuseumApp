a6f5881e99e19ab7d7a51ac422f5a85f5bd439a9
Paolo Milano <spacitron999@gmail.com>
2020-06-21 20:54:00 +0200

    Enabled data binding

---
+++ app/src/main/java/com/magicbluepenguin/rijksmuseumapp/rijksartobjectlist/RijksArtObjectListAdapter.kt
@@ -0,0 +1,46 @@
+package com.magicbluepenguin.rijksmuseumapp.rijksartobjectlist
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagedListAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.magicbluepenguin.rijksmuseumapp.data.RijksArtObject
+import com.magicbluepenguin.rijksmuseumapp.databinding.ItemViewArtObjectBinding
+
+internal class RijksArtObjectItem(private val itemViewArtObjectBinding: ItemViewArtObjectBinding) :
+    RecyclerView.ViewHolder(itemViewArtObjectBinding.root) {
+    fun bind(rijksArtObject: RijksArtObject) {
+        itemViewArtObjectBinding.artObject = rijksArtObject
+    }
+}
+
+internal class RijksArtObjectListAdapter(private val artObjectSelectionListener: (RijksArtObject) -> Unit) :
+    PagedListAdapter<RijksArtObject, RijksArtObjectItem>(DIFF_CALLBACK) {
+
+    override fun onBindViewHolder(holder: RijksArtObjectItem, position: Int) {
+        val item = getItem(position)
+        if (item != null) {
+            holder.bind(item)
+            holder.itemView.setOnClickListener {
+                artObjectSelectionListener.invoke(item)
+            }
+        } else {
+            // TODO Show placeholder
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        RijksArtObjectItem(ItemViewArtObjectBinding.inflate(LayoutInflater.from(parent.context)))
+
+    companion object {
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<RijksArtObject>() {
+
+            override fun areItemsTheSame(oldItem: RijksArtObject, newItem: RijksArtObject) =
+                oldItem == newItem
+
+            override fun areContentsTheSame(oldItem: RijksArtObject, newItem: RijksArtObject) =
+                areItemsTheSame(oldItem, newItem)
+        }
+    }
+}
--- app/src/main/java/com/magicbluepenguin/rijksmuseumapp/rijksartobjectlist/RijksArtObjectListFragment.kt
+++ app/src/main/java/com/magicbluepenguin/rijksmuseumapp/rijksartobjectlist/RijksArtObjectListFragment.kt
@@ -4,69 +4,47 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.lifecycle.Observer
-import androidx.paging.PagedListAdapter
-import androidx.recyclerview.widget.DiffUtil
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.magicbluepenguin.rijksmuseumapp.R
-import com.magicbluepenguin.rijksmuseumapp.data.RijksArtObject
-import kotlinx.android.synthetic.main.fragment_art_object_list.*
+import com.magicbluepenguin.rijksmuseumapp.base.BaseFragment
+import com.magicbluepenguin.rijksmuseumapp.dagger.rijksartobjectcomponent.RijksMuseumAppComponent
+import com.magicbluepenguin.rijksmuseumapp.databinding.FragmentArtObjectListBinding
 
-internal class RijksArtObjectListFragment : BaseFragment(R.layout.fragment_art_object_list) {
+internal class RijksArtObjectListFragment : BaseFragment() {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    val artObjectListViewModel by viewModels<RijksArtObjectListViewModel>()
 
-        val adapter = RijksArtObjectAdapter()
-        artObjectRecyclerView.layoutManager = LinearLayoutManager(context)
-        artObjectRecyclerView.adapter = adapter
-
-        val rijksArtObjectListViewModel by viewModelso<RijksArtObjectListViewModel>()
-        rijksArtObjectListViewModel.loadData()
-        rijksArtObjectListViewModel.rijksArtObjectListLiveData.observe(
-            viewLifecycleOwner,
-            Observer {
-                artObjectSwipeRefresh.isRefreshing = false
-                adapter.submitList(it)
-            }
-        )
-        artObjectSwipeRefresh.setOnRefreshListener { rijksArtObjectListViewModel.loadData() }
+    override fun onAppComponentReady(rijksMuseumAppComponent: RijksMuseumAppComponent) {
+        rijksMuseumAppComponent.getArtObjectListSubcomponent()
+            .build()
+            .inject(this)
     }
 
-    class RijksArtObjectItem(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(rijksArtObject: RijksArtObject) {
-            itemView.findViewById<TextView>(R.id.itemViewArtObjectTitle).text = rijksArtObject.title
-        }
-    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return FragmentArtObjectListBinding.inflate(inflater).apply {
+            rijksArtObjectListViewModel = artObjectListViewModel
 
-    class RijksArtObjectAdapter :
-        PagedListAdapter<RijksArtObject, RijksArtObjectItem>(DIFF_CALLBACK) {
-
-        override fun onBindViewHolder(holder: RijksArtObjectItem, position: Int) {
-            val item = getItem(position)
-            if (item != null) {
-                holder.bind(item)
-            } else {
-                // Show placeholder
+            val pagedArtObjectAdapter = RijksArtObjectListAdapter {
+                RijksArtObjectListFragmentDirections.actionRijksArtObjectListFragmentToRijksArtObjectDetailFragment(
+                    it.objectNumber
+                ).let {
+                    findNavController().navigate(it)
+                }
             }
-        }
+            artObjectRecyclerView.adapter = pagedArtObjectAdapter
+            artObjectRecyclerView.layoutManager = LinearLayoutManager(requireActivity())
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = RijksArtObjectItem(
-            LayoutInflater.from(parent.context)
-                .inflate(R.layout.item_view_art_object, parent, false)
-        )
-
-        companion object {
-            private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<RijksArtObject>() {
-
-                override fun areItemsTheSame(oldItem: RijksArtObject, newItem: RijksArtObject) =
-                    oldItem == newItem
-
-                override fun areContentsTheSame(oldItem: RijksArtObject, newItem: RijksArtObject) =
-                    areItemsTheSame(oldItem, newItem)
-            }
-        }
+            artObjectListViewModel.rijksArtObjectListLiveData.observe(
+                viewLifecycleOwner,
+                Observer {
+                    pagedArtObjectAdapter.submitList(it)
+                }
+            )
+        }.root
     }
 }
